55974ce1389077d55af26bfa9b0851036de4be7a
Rosemaryefebe247 <efebeoghenekaro@gmail.com>
2023-07-31 06:23:46 +0100

    AirBnBv3

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,34 +1,14 @@
 #!/usr/bin/python3
-'''
-    flask with general routes
-    routes:
-        /status:    display "status":"OK"
-        /stats:     dispaly total for all classes
-'''
 from api.v1.views import app_views
 from flask import jsonify
-from models import storage
 
-
-@app_views.route("/status")
-def status():
-    '''
-        return JSON of OK status
-    '''
-    return jsonify({'status': 'OK'})
-
-
-@app_views.route("/stats")
-def storage_counts():
-    '''
-        return counts of all classes in storage
-    '''
-    cls_counts = {
-        "amenities": storage.count("Amenity"),
-        "cities": storage.count("City"),
-        "places": storage.count("Place"),
-        "reviews": storage.count("Review"),
-        "states": storage.count("State"),
-        "users": storage.count("User")
+@app_views.route('/status')
+def get_status():
+    """
+    Function that returns status Ok
+    return the response in json format
+    """
+    response = {
+        "status": "OK"
     }
-    return jsonify(cls_counts)
+    return jsonify(response)
